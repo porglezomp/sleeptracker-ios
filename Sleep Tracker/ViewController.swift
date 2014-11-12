30db0f0255422cf2f4a27049d0fe3d40428697e7
Caleb Jones <bionicle.dragon@gmail.com>
2014-11-11 21:38:26 -0500

    Initial Commit

---
+++ Sleep Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sleep Tracker
+//
+//  Created by Caleb Jones on 11/11/14.
+//  Copyright (c) 2014 Caleb Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sleep TrackerTests/Sleep_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sleep_TrackerTests.swift
+//  Sleep TrackerTests
+//
+//  Created by Caleb Jones on 11/11/14.
+//  Copyright (c) 2014 Caleb Jones. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sleep_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
