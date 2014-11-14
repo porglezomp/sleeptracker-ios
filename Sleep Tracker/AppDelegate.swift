0d666ca7bb43a5d9f4c2f644136ec31fba8ad92a
Caleb Jones <bionicle.dragon@gmail.com>
2014-11-14 00:37:05 -0500

    Improve interface visuals
    Change spacing
    Add gradient on launch screen
    Change status bar color for launch screen

--- Sleep Tracker/AppDelegate.swift
+++ Sleep Tracker/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        application.setStatusBarStyle(UIStatusBarStyle.BlackOpaque, animated: true)
         // Override point for customization after application launch.
         
         return true
